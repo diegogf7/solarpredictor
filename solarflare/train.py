57c6b4355c7df307c9c41c2b694c3ad37ab05e99
diegogf7 <diego.gaf28@gmail.com>
2026-06-04 21:21:39 -0400

    Final training code with checkpoint saving

--- solarflare/train.py
+++ solarflare/train.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from dataset import SolarFlareDataset, transform
 from model import SolarCNN
+from tqdm import tqdm
+
 
 
 device = torch.device('mps')
@@ -16,8 +18,8 @@ if __name__ == '__main__':
     training_dataset = SolarFlareDataset(TRAIN_CSV, IMAGES_DIR, transform = transform)
     validation_dataset = SolarFlareDataset(VAL_CSV, IMAGES_DIR, transform = transform)
 
-    train_loader = DataLoader(training_dataset, batch_size = 32, shuffle = True, num_workers = 8)
-    validation_loader = DataLoader(validation_dataset, batch_size = 32, shuffle = False, num_workers = 8)
+    train_loader = DataLoader(training_dataset, batch_size = 32, shuffle = True, num_workers = 0)
+    validation_loader = DataLoader(validation_dataset, batch_size = 32, shuffle = False, num_workers = 0)
 
     model = SolarCNN().to(device)
     positive_weight = torch.tensor([31.2]).to(device) #had to calculate when we get a class 0 vs class 1 solar flare
@@ -28,17 +30,20 @@ if __name__ == '__main__':
         model.train()
         running_loss = 0.0
 
-        for image, labels in train_loader:
+        progress = tqdm(train_loader, desc=f'Epoch {epoch+1}/10')
+        for image, labels in progress:
             image = image.to(device)
             labels = labels.to(device)
 
             optimizer.zero_grad()
-            outputs = model(image).squeeze(1) #what does this line do?
+            outputs = model(image).squeeze(1)
             loss = criteria(outputs, labels)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-        
+            progress.set_postfix(loss=f'{loss.item():.4f}')
+
         average_loss = running_loss / len(train_loader)
-        print("Loss:", average_loss)
+        print(f'Epoch {epoch+1}/10 complete — Avg Loss: {average_loss:.4f}')
+        torch.save(model.state_dict(), f'checkpoint_epoch{epoch+1}.pth')
